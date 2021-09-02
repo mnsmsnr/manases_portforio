a301f009a993563ff8a28fd4240fad7c0f8504d4
mnsmsnr <vanillasky0127@yahoo.co.jp>
2021-09-02 16:36:30 +0900

    skillにDraggableListサンプル導入

--- src/page/skill.js
+++ src/page/skill.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import { DragList } from '../useSprings/dragList.tsx';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+    test: {
+        width:'50%',
+        float:'left',
+    },
+  }));
 
 export const Skill = () => {
+    const classes = useStyles();
     return (
         <div>
-            skill<br/>
+            <div className={classes.test}>
+                <DragList />
+            </div>
+            <div className={classes.test}>
+                <DragList />
+            </div>
         </div>
     );
 };
