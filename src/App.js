5902a62a88fd0eb02b2d07e82ce11e0084b7012f
mnsmsnr <vanillasky0127@yahoo.co.jp>
2021-09-09 15:23:47 +0900

    エントリー制御修正

--- src/App.js
+++ src/App.js
@@ -1,20 +1,30 @@
 import React from 'react';
-import { Route, Switch, withRouter } from 'react-router-dom';
+import { Route, Switch, BrowserRouter } from 'react-router-dom';
 import { Routes } from './routes';
+import { Header } from './header';
+import { Footer } from './footer';
 
-const App = () => {
-  return (
+const Contents = () => { 
+  return(
   <Switch>
-    {Routes.map((route, idx) => (
-      <Route
-        path={route.path}
-        exact={route.exact}
-        component={route.component}
-        key={idx}
-      />
-    ))}
-  </Switch>
+      {Routes.map((route, idx) => (
+        <Route
+          path={route.path}
+          exact={route.exact}
+          component={route.component}
+          key={idx}
+        />
+      ))}
+    </Switch>
+  )
+}
+
+export const App = () => {
+  return (    
+      <BrowserRouter>
+        <Header />
+        <Contents />
+        <Footer />
+      </BrowserRouter>
   );
 };
-
-export default withRouter(App);
